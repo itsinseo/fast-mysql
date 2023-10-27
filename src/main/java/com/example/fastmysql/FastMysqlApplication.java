c5237cb722c1eec5d20572af51b30a08ebf5b6ef
Inseo Kim <itsinseo@gmail.com>
2023-10-27 19:38:46 +0900

    git init

---
+++ src/main/java/com/example/fastmysql/FastMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastMysqlApplication.class, args);
+    }
+
+}
